20a92803bd324bebd82e7d765485fb7f550439cc
StefanMuijs <145674829+StefanMuijs@users.noreply.github.com>
2025-04-04 15:02:36 +0200

    updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,31 @@
 import './index.css'
-import {createBrowserRouter, RouterProvider} from "react-router";
-import Layout from "./Layout.jsx";
-import Home from "./Home.jsx";
-import About from "./About.jsx";
-import OpenHiring from "./showcase_pages/OpenHiring.jsx";
-import SOTD from "./showcase_pages/SOTD.jsx";
-import GaiaPark from "./showcase_pages/GaiaPark.jsx";
-import SpeakSilent from "./showcase_pages/SpeakSilent.jsx";
-import Excalibur from "./showcase_pages/Excalibur.jsx";
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from './Home.jsx';
+import About from './About.jsx';
+import OpenHiring from './showcase_pages/OpenHiring.jsx';
+import SOTD from './showcase_pages/SOTD.jsx';
+import GaiaPark from './showcase_pages/GaiaPark.jsx';
+import SpeakSilent from './showcase_pages/SpeakSilent.jsx';
+import Excalibur from './showcase_pages/Excalibur.jsx';
 
-const router = createBrowserRouter([{
-    element: <Layout />,
-    children: [
-        {
-            path: "/",
-            element: <Home/>
-        },
-        {
-            path: "/about",
-            element: <About/>
-        },
-        {
-            path:"/openhiring",
-            element: <OpenHiring/>
-        },
-        {
-            path:"/sotd",
-            element: <SOTD/>
-        },
-        {
-            path:"/gaiapark",
-            element: <GaiaPark/>
-        },
-        {
-            path:"/speaksilent",
-            element: <SpeakSilent/>
-        },
-        {
-            path:"/excalibur",
-            element: <Excalibur/>
-        },
-
-    ]
-}])
+const router = createHashRouter([
+    {
+        element: <Layout />,
+        children: [
+            { path: '/', element: <Home /> },
+            { path: '/about', element: <About /> },
+            { path: '/openhiring', element: <OpenHiring /> },
+            { path: '/sotd', element: <SOTD /> },
+            { path: '/gaiapark', element: <GaiaPark /> },
+            { path: '/speaksilent', element: <SpeakSilent /> },
+            { path: '/excalibur', element: <Excalibur /> },
+        ]
+    }
+]);
 
 function App() {
-
-    return (
-        <>
-            <RouterProvider router={router}/>
-        </>
-    )
+    return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
